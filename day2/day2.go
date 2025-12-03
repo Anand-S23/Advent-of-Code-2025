1b25e54e1ebe5a83df23da2f8b33e718228b48d7
Anand-S23 <anandsroopsingh23@gmail.com>
2025-12-02 23:08:10 -0500

    Day 2 - Part 2

--- day2/day2.go
+++ day2/day2.go
@@ -8,6 +8,45 @@ import (
 	"strings"
 )
 
+func split_n(s string, n int) []string {
+    if n <= 0 {
+        return nil
+    }
+
+    partSize := (len(s) + n - 1) / n // ceiling division
+    parts := []string{}
+
+    for i := 0; i < len(s); i += partSize {
+        end := i + partSize
+        if end > len(s) {
+            end = len(s)
+        }
+        parts = append(parts, s[i:end])
+    }
+
+    return parts
+}
+
+func is_invalid(id string) bool {
+	for i := 2; i < len(id) + 1; i++ {
+		split := split_n(id, i)
+		all_equal := true
+		for _, v := range split {
+			//println(id, i, v, split[0])
+			if v != split[0] {
+				all_equal = false
+
+			}
+		}
+
+		if all_equal {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	f, err := os.Open("input.txt")
 	if err != nil {
@@ -42,11 +81,8 @@ func main() {
 
 		for current_id := start; current_id < end + 1; current_id++ {
 			current_id_str := strconv.Itoa(current_id)
-			if len(current_id_str) % 2 == 0 {
-				half := len(current_id_str) / 2
-				if current_id_str[:half] == current_id_str[half:] {
-					res += current_id
-				}
+			if is_invalid(current_id_str) {
+				res += current_id
 			}
 		}
 	}
