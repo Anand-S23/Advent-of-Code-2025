b88c8ca62bf22ee1a20e6034cf3abb928fe03c38
Anand-S23 <anandsroopsingh23@gmail.com>
2025-12-04 20:09:43 -0500

    Day 3 - Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -38,6 +38,36 @@ func part1(input []string) int {
 	return res
 }
 
+func part2(input []string) int {
+	res := 0
+
+	for _, line := range input {
+		current_place := 100000000000
+		start_index := 0
+		current_joltage := 0
+
+		for i := 1; i < 13; i++ {
+			max_battery := -1
+			battery_index := -1
+			for j := start_index; j < (len(line) - (12 - i)); j++ {
+				current_battery := to_int(string(line[j]))
+				if current_battery > max_battery {
+					max_battery = current_battery
+					battery_index = j
+				}
+			}
+
+			start_index = battery_index + 1
+			current_joltage += max_battery * current_place
+			current_place /= 10
+		}
+
+		res += current_joltage
+	}
+
+	return res
+}
+
 func main() {
 	f, err := os.Open("input.txt")
 	if err != nil {
@@ -58,4 +88,7 @@ func main() {
 
 	part1_res := part1(lines)
 	println("Part 1 Result:", part1_res)
+
+	part2_res := part2(lines)
+	println("Part 2 Result:", part2_res)
 }
